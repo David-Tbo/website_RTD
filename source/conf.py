409c3ffe56038ffd320d851f4e2bd10fde191b43
David-Tbo <davidtbo@outlook.fr>
2026-04-06 17:09:43 +0200

    Empty and review the repository

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+
+# Project information
+project = 'website Machine Learning'
+author = 'David TBO'
+release = '0.1'
+
+# Sphinx extensions
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+]
+
+# Paths to the templates and files to exclude
+templates_path = ['_templates']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# HTML
+html_theme = 'sphinx_rtd_theme'
+
+# nbsphinx configuration
+nbsphinx_execute = 'never'
+nbsphinx_allow_errors = True
+
+# Markdown supports and Notebooks
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. website Machine Learning documentation master file, created by
+   sphinx-quickstart on Sun Jul  6 23:33:51 2025.
+
+Welcome to this Data Science documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   probabilities/index
+   statistics/index
+   python/index
+   machine_learning/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
